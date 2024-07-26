853d94a070ebf111b43756eb092570e68f9b0609
AnikaSood28 <teerthmittal05@gmail.com>
2024-07-26 21:52:29 +0530

    some minor changes

--- forecast.test.js
+++ forecast.test.js
@@ -1,5 +1,3 @@
-
-
 const request = require('postman-request');
 const { fetchWeatherData } = require('./src/utils/forecast'); 
 
@@ -13,20 +11,24 @@ describe('System Setup', () => {
             main: { temp: 300, feels_like: 295, humidity: 80 },
             wind: { speed: 5 },
             dt: Date.now(),
-            cod: 200 
+            cod: 200,
+            name: 'Delhi'
         };
 
         request.mockImplementation((options, callback) => {
             callback(null, { body: mockData });
         });
 
-       
         fetchWeatherData('Delhi', (error, data) => {
-            expect(error).toBeNull();
-            expect(data).toBeDefined();
-            expect(data.city).toBe('Delhi');
-            expect(data.temp).toBeCloseTo(26.85, 2); 
+            try {
+                expect(error).toBeNull();
+                expect(data).toBeDefined();
+                expect(data.city).toBe('Delhi');
+                expect(data.temp).toBeCloseTo(26.85, 2); 
+                done();
+            } catch (err) {
+                done(err);
+            }
         });
-    });
+    }, 10000); // Increase timeout to 10 seconds
 });
-
